4c0ed6eec82093ab7524e812709f7c013a891c4c
FelixOz13 <94942094+FelixOz13@users.noreply.github.com>
2023-04-14 02:14:02 -0600

    first commit

---
+++ server/sampleData.js
@@ -0,0 +1,66 @@
+const projects = [
+   {
+    id: '1',
+    clientId: '1',
+    name: 'eCommerce Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Dating App',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'SEO Project',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Design Prototype',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'Done',
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'Auction Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+]
+ 
+
+ const clients = [
+    { 
+       id: '1',
+       name: 'Felix Ozuna',
+       email: 'felixozuna@outlook.com',
+       username: 'FelixOz13', 
+    },
+     { 
+       id: '2',
+       name: 'Felix Casillas',
+       email: 'felixozuna13@gmail.com',
+       username: 'UltimateWarrior', 
+    },
+     { 
+       id: '3',
+       name: 'Felix  Ozuna Casillas',
+       email: 'felixozuna13@icloud.com',
+       username: 'Felixozuna13', 
+   } ,
+ ]
+          
+module.exports = { projects, clients };
